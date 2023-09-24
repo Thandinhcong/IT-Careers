3d1fc8805bfd20a7694562e0efbb57b67618420f
huuthedinh <thedh081003@gmail.com>
2023-09-24 21:55:00 +0700

    fix

--- FE/src/component/Account/ForgotPass.tsx
+++ FE/src/component/Account/ForgotPass.tsx
@@ -4,15 +4,16 @@ const ForgotPass = () => {
     return (
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-lg">
-                <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
-                    Bạn Đã Quên Mật Khẩu ?
-                </h1>
+
 
 
                 <form
                     action=""
                     className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
                 >
+                    <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
+                        Bạn Đã Quên Mật Khẩu ?
+                    </h1>
                     <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
                         Đừng lo, chúng tôi sẽ gửi cho bạn một tin nhắn giúp bạn đặt lại mật khẩu qua email đăng ký tài khoản.
                     </p>
--- FE/src/component/Account/SignUp.tsx
+++ FE/src/component/Account/SignUp.tsx
@@ -9,13 +9,14 @@ const SignUp = () => {
     return (
         <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-lg">
-                <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
-                    Chào Mừng Đến Với IT Careers
-                </h1>
+
                 <form
                     action=""
                     className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
                 >
+                    <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
+                        Chào Mừng Đến Với IT Careers
+                    </h1>
                     <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
                         Cùng xây dựng một hồ sơ nổi bật và nhận được các cơ hội sự nghiệp lý tưởng tại IT Careers nhé !!!
                     </p>
