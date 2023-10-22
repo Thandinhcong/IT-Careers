8ff9626903ca6035b2b788f23a872a77d685ddab
huuthedinh <thedh081003@gmail.com>
2023-10-22 14:33:45 +0700

    list jobs & jobsDetail

--- FE/src/pages/main/Recruitment/index.tsx
+++ FE/src/pages/main/Recruitment/index.tsx
@@ -11,43 +11,43 @@ const Recruitment = () => {
     console.log(listJobs);
 
     return (
-        // <div>
-        //     <div className='mb-10   lg:p-1'>
-        //         <div className='flex justify-between'>
-        //             <h2 className='font-bold md:text-2xl'>
-        //                 Tuyển dụng, việc làm
-        //                 <span className='text-blue-500'> tốt nhất</span>
-        //             </h2>
-        //             <Link to="" className='flex items-center gap-2  hover:text-blue-500'>Xem tất cả  <BsArrowRight /></Link>
-        //         </div>
-        //         <div className='my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 '>
-        //             {listJobs?.map((item: IListJobs) => {
-        //                 if (item.status === 0 || item.status === 2) {
-        //                     return null;
-        //                 } else {
-        //                     return (
-        //                         <Link to={`job-detail/${item?.id}`} className='shadow-lg p-2' key={item?.id}>
-        //                             <div className='flex gap-2'>
-        //                                 <img src={item?.logo} className='border rounded-md p-2' width={70} />
-        //                                 <div>
-        //                                     <Link to="/">
-        //                                         <p className='text-slate-500 font-semibold text-base'>{item?.title}</p>
-        //                                     </Link>
-        //                                     <p>{item?.company_name}</p>
-        //                                 </div>
-        //                             </div>
-        //                             <p className='flex items-center gap-1 my-2'> <MdRoom /> <span>{item?.area}</span> </p>
-        //                             <div className='flex justify-between items-center mb-2'>
-        //                                 <p className='flex items-center gap-1'> <BsCurrencyDollar /><span>{item.min_salary} - {item.max_salary}</span></p>
-        //                                 <i className='border p-1'><MdFavoriteBorder /></i>
-        //                             </div>
-        //                         </Link>
-        //                     );
-        //                 }
-        //             })}
-        //         </div>
-        //     </div>
-        // </div>
+        <div>
+            <div className='mb-10   lg:p-1'>
+                <div className='flex justify-between'>
+                    <h2 className='font-bold md:text-2xl'>
+                        Tuyển dụng, việc làm
+                        <span className='text-blue-500'> tốt nhất</span>
+                    </h2>
+                    <Link to="" className='flex items-center gap-2  hover:text-blue-500'>Xem tất cả  <BsArrowRight /></Link>
+                </div>
+                <div className='my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 '>
+                    {listJobs?.map((item: IListJobs) => {
+                        if (item.status === 0 || item.status === 2) {
+                            return null;
+                        } else {
+                            return (
+                                <Link to={`job-detail/${item?.id}`} className='shadow-lg p-2' key={item?.id}>
+                                    <div className='flex gap-2'>
+                                        <img src={item?.logo} className='border rounded-md p-2' width={70} />
+                                        <div>
+                                            <Link to="/">
+                                                <p className='text-slate-500 font-semibold text-base'>{item?.title}</p>
+                                            </Link>
+                                            <p>{item?.company_name}</p>
+                                        </div>
+                                    </div>
+                                    <p className='flex items-center gap-1 my-2'> <MdRoom /> <span>{item?.area}</span> </p>
+                                    <div className='flex justify-between items-center mb-2'>
+                                        <p className='flex items-center gap-1'> <BsCurrencyDollar /><span>{item.min_salary} - {item.max_salary}</span></p>
+                                        <i className='border p-1'><MdFavoriteBorder /></i>
+                                    </div>
+                                </Link>
+                            );
+                        }
+                    })}
+                </div>
+            </div>
+        </div>
     )
 }
 
