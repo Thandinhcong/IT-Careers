a0a7bea9b9c23ab9b278c972aa8d1d3e1c260973
thandinhjs <dinhcongthan03@gmail.com>
2023-10-27 10:53:14 +0700

    fix:bug không hiển thị

--- FE/src/app/store.ts
+++ FE/src/app/store.ts
@@ -74,7 +74,7 @@ const middleware = [
     companysApi.middleware,
     jobsApi.middleware,
     AdminLogin.middleware,
-    CompnayInfoApi.middleware
+    CompnayInfoApi.middleware,
     JobPostCompanyApi.middleware,
 ]
 
