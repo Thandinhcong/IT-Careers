2600914fdfb26519115945958475137e4f2dc06f
thandinhjs <dinhcongthan03@gmail.com>
2023-11-28 01:16:08 +0700

    Area work top

--- FE/src/pages/main/TopArea/index.tsx
+++ FE/src/pages/main/TopArea/index.tsx
@@ -1,37 +1,29 @@
-import { Link } from 'react-router-dom'
+import { useGetAreaAllQuery } from '../../../api/areaApi'
 
 const TopArea = () => {
+    const { data } = useGetAreaAllQuery();
+    const listAreaTop = data?.job_list;
+    const sortedListAreaTop = listAreaTop?.sort((a: any, b: any) => b.job_count - a.job_count) || [];
+
     return (
         <div className='mb-10 rounded-2xl '>
             <h2 className='text-2xl'>Các khu vực <span className='text-blue-500'> hàng đầu</span></h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
-                <div className='shadow-md mt-10'>
-                    <img
-                        src="https://media.vietravel.com/images/news/ha-noi-1.jpg"
-                        className='rounded-t-2xl mb-2'
-                        alt=""
-                    />
-                    <Link to="" className='font-semibold p-2 my-3 hover:text-blue-500'>Hà Nội</Link>
-                    <p className='p-2'>36.549 việc làm</p>
-                </div>
-                <div className='shadow-md mt-10'>
-                    <img
-                        src="https://media.vietravel.com/images/news/ha-noi-1.jpg"
-                        className='rounded-t-2xl mb-2'
-                        alt=""
-                    />
-                    <Link to="" className='font-semibold p-2 my-3  hover:text-blue-500'>Hồ Chí Minh</Link>
-                    <p className='p-2'>36.549 việc làm</p>
-                </div>
-                <div className='shadow-md mt-10'>
-                    <img
-                        src="https://media.vietravel.com/images/news/ha-noi-1.jpg"
-                        className='rounded-t-2xl mb-2'
-                        alt=""
-                    />
-                    <Link to="" className='font-semibold p-2 my-3 hover:text-blue-500'>Đà Nẵng</Link>
-                    <p className='p-2'>36.549 việc làm</p>
-                </div>
+                {sortedListAreaTop?.map((item: any) => {
+                    return (
+                        <div key={item?.id} className='shadow-md mt-10'>
+                            <img
+                                src="https://media.vietravel.com/images/news/ha-noi-1.jpg"
+                                className='rounded-t-2xl mb-2'
+                                alt=""
+                            />
+                            <div className='font-semibold px-2 my-2 hover:text-blue-500'>{item?.province}</div>
+                            <p className='p-2'>{item?.job_count
+                            } việc làm</p>
+                        </div>
+                    )
+                })}
+
             </div>
         </div>
     )
