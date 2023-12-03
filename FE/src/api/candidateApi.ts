b96e1a316c5cafbaa13474c70a7d80ba7a83a96d
datlqph24221 <lequocdat2312@gmail.com>
2023-12-03 22:06:56 +0700

    fix Account manage

--- FE/src/api/candidateApi.ts
+++ FE/src/api/candidateApi.ts
@@ -1,42 +1,63 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { ICompanyInfor } from "../interfaces";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { IAccount } from "../interfaces";
 
-// const candidateApi = createApi({
-//     reducerPath: "Candidate",
-//     tagTypes: ['candidate'],
-//     baseQuery: fetchBaseQuery({
-//         baseUrl: "http://127.0.0.1:8000/api",
-//         fetchFn: async (...arg) => {
-//             return fetch(...arg)
-//         }
-//     }),
-//     endpoints: (builder) => ({
-//         getCompanySetting: builder.query<ICompanyInfor[], void>({
-//             query: () => "/list_company",
-//             providesTags: ['candidate']
-//         }),
-//         getCompanySettingById: builder.query<ICompanyInfor, number | string>({
-//             query: (id) => "/list_company/" + id,
-//             providesTags: ['candidate']
-//         }),
 
-//         editCompanySetting: builder.mutation<ICompanyInfor, ICompanyInfor>({
-//             query: (company: ICompanyInfor) => ({
-//                 url: `/list_company/${company.id}`,
-//                 method: "PUT",
-//                 body: company
-//             }),
-//             invalidatesTags: ['candidate']
-//         }),
+export interface IAccountAll {
+    status?: string,
+    data: IAccount[]
+}
+const CandidateApi = createApi({
+    reducerPath: "candidate_information",
+    tagTypes: ['candidates'],
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_API_ADMIN,
 
-//     })
-// })
-// export const {
-//     useGetCompanySettingQuery,
-//     useGetCompanySettingByIdQuery,
-//     useEditCompanySettingMutation,
-// } = candidateApi;
+        prepareHeaders: (headers) => {
+            const user = JSON.parse(localStorage.getItem("admin") as string);
+            const token = user?.accessToken;
+            if (token) {
+                headers.set('authorization', `Bearer ${token}`)
+            }
+            return headers
+        }
+    }),
+    endpoints: (builder) => ({
+        getCandidates: builder.query<any, void>({
+            query: () => "/candidate",
+            providesTags: ['candidates']
+        }),
+        editCandidate: builder.mutation({
+            query: (candidate: IAccount) => ({
+                url: `/candidate/`,
+                method: "POST",
+                body: candidate
+            }),
+            invalidatesTags: ['candidates']
+        }),
+        changePassCandidate: builder.mutation({
+            query: (candidate: IAccount) => ({
+                url: `/refreshPass`,
+                method: "POST",
+                body: candidate
+            }),
+            invalidatesTags: ['candidates']
+        }),
+        deleteCandidate: builder.mutation<void, number | string>({
+            query: (id) => ({
+                url: `/candidates/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ['candidates']
+        }),
+    })
+})
+export const {
+    useGetCandidatesQuery,
+    useEditCandidateMutation,
+    useDeleteCandidateMutation,
+    useChangePassCandidateMutation
+} = CandidateApi;
 
-// export const candidateReducer = candidateApi.reducer;
+export const CandidateReducer = CandidateApi.reducer;
 
-// export default candidateApi;
+export default CandidateApi;
